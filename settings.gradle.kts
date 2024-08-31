03a9c495719abaa08a773dc8a06ec3eddd21a25c
xiaoshen <xiaoshen@endoqa.net>
2024-08-31 15:00:11 -0400

    start new work on LLVM

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,17 @@
 pluginManagement {
-    repositories {
-        mavenCentral()
-        google()
-        gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
-        maven("https://www.jetbrains.com/intellij-repository/releases")
-        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
-    }
+  repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+    maven("https://packages.jetbrains.team/maven/p/amper/amper")
+    maven("https://www.jetbrains.com/intellij-repository/releases")
+    maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+  }
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.3.2-dev-582")
+  id("org.jetbrains.amper.settings.plugin").version("0.5.0-dev-992")
 }
+
+include(":libLLVM")
+include(":llvm")
