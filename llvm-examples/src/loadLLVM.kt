e7cabdcd1ed828d10d78fb3568a21a9970c8ef9e
xiaoshen <xiaoshen@endoqa.net>
2024-09-26 21:15:52 -0400

    better loadLLVM

--- llvm-examples/src/loadLLVM.kt
+++ llvm-examples/src/loadLLVM.kt
@@ -1,7 +1,36 @@
 import java.io.File
 
+
+private enum class Os(
+    val libPrefix: String,
+    val libExtension: String,
+    val binPath: String
+) {
+    Linux("lib", ".so", "lib"),
+    Win("", ".dll", "bin");
+
+
+    fun libName(name: String) = "${libPrefix}${name}${libExtension}"
+}
+
 fun loadLLVM() {
-    System.load(File("/nix/store/35mrxhx9vwf3ms734xydxfrh3d84d9fn-llvm-18.1.8-lib/lib/libLLVM.so").absolutePath)
-    System.load(File("/nix/store/35mrxhx9vwf3ms734xydxfrh3d84d9fn-llvm-18.1.8-lib/lib/libRemarks.so").absolutePath)
-    System.load(File("/nix/store/35mrxhx9vwf3ms734xydxfrh3d84d9fn-llvm-18.1.8-lib/lib/libLTO.so").absolutePath)
+    val os = getOS()
+
+    val llvmPath = System.getenv("LLVM_DEV_PATH") + "/" + os.binPath
+
+
+    System.load(File("${llvmPath}/${os.libName("LLVM")}").absolutePath)
+    System.load(File("${llvmPath}/${os.libName("Remarks")}").absolutePath)
+    System.load(File("${llvmPath}/${os.libName("LTO")}").absolutePath)
+}
+
+private fun getOS(): Os {
+
+    val osName = System.getProperty("os.name")
+
+    return when {
+        osName.startsWith("Win") -> Os.Win
+        osName.contains("Linux") -> Os.Linux
+        else -> error("Unknown OS")
+    }
 }
